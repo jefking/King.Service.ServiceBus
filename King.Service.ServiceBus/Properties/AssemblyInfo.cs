d15f71ca2c1e0bccdbb8590960e370a709638b51
Jef King <jef@agilebusinesscloud.com>
2014-11-15 20:54:47 -0800

    Init

---
+++ King.Service.ServiceBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Service.ServiceBus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Service.ServiceBus")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dfbd1f01-e684-44cd-b931-8ae84d77f423")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,9 @@
+### Task scheduling for Azure and Windows
+
+### [NuGet](https://www.nuget.org/packages/King.ServiceBus)
+```
+PM> Install-Package King.Service.ServiceBus
+```
+
+### [Wiki](https://github.com/jefking/King.ServiceBus/wiki)
+View the wiki to learn how to use this.
