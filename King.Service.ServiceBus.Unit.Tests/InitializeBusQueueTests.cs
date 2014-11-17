dc2a606d9cfc50e332c7603d6e16e474f2e5c33d
Jef King <jef@agilebusinesscloud.com>
2014-11-16 16:27:12 -0800

    Tests

--- King.Service.ServiceBus.Unit.Tests/InitializeBusQueueTests.cs
+++ King.Service.ServiceBus.Unit.Tests/InitializeBusQueueTests.cs
@@ -4,9 +4,42 @@
     using NSubstitute;
     using NUnit.Framework;
     using System;
+    using System.Threading.Tasks;
 
     [TestFixture]
     public class InitializeBusQueueTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var queue = Substitute.For<IBusQueue>();
+            new InitializeBusQueue(queue);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorQueueNull()
+        {
+            new InitializeBusQueue(null);
+        }
+
+        [Test]
+        public void IsInitializeTask()
+        {
+            var queue = Substitute.For<IBusQueue>();
+            Assert.IsNotNull(new InitializeBusQueue(queue) as InitializeTask);
+        }
+
+        [Test]
+        public async Task RunAsync()
+        {
+            var queue = Substitute.For<IBusQueue>();
+            queue.CreateIfNotExists();
+
+            var t = new InitializeBusQueue(queue);
+            await t.RunAsync();
+
+            queue.Received().CreateIfNotExists();
+        }
     }
 }
