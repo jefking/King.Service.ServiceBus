0069bad13ee96547ade3e87fdcce65713cb09736
Jef King <jef@agilebusinesscloud.com>
2014-11-16 16:24:54 -0800

    Tests

--- King.Service.ServiceBus.Unit.Tests/QueuedTests.cs
+++ King.Service.ServiceBus.Unit.Tests/QueuedTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace King.Service.ServiceBus.Unit.Tests
 {
     using King.Service.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
     using NSubstitute;
     using NUnit.Framework;
     using System;
@@ -8,5 +9,19 @@
     [TestFixture]
     public class QueuedTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var msg = new BrokeredMessage();
+            new Queued<object>(msg);
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorMessageNull()
+        {
+            new Queued<object>(null);
+        }
     }
 }
