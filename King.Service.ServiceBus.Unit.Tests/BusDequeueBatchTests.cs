47b18c8602c966e70a13e2069c2f85b88c60c1d3
Jef King <jef@agilebusinesscloud.com>
2014-11-16 16:39:46 -0800

    Unit Testing

--- King.Service.ServiceBus.Unit.Tests/BusDequeueBatchTests.cs
+++ King.Service.ServiceBus.Unit.Tests/BusDequeueBatchTests.cs
@@ -1,12 +1,28 @@
 ﻿namespace King.Service.ServiceBus.Unit.Tests
 {
+    using King.Azure.Data;
+    using King.Service.Data;
     using King.Service.ServiceBus;
     using NSubstitute;
     using NUnit.Framework;
-    using System;
 
     [TestFixture]
     public class BusDequeueBatchTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            var queue = Substitute.For<IBusQueue>();
+            new BusDequeueBatch<object>(queue, processor);
+        }
+
+        [Test]
+        public void IsDequeueBatch()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            var queue = Substitute.For<IBusQueue>();
+            Assert.IsNotNull(new BusDequeueBatch<object>(queue, processor) as DequeueBatch<object>);
+        }
     }
 }
--- King.Service.ServiceBus.Unit.Tests/BusDequeueTests.cs
+++ King.Service.ServiceBus.Unit.Tests/BusDequeueTests.cs
@@ -1,12 +1,28 @@
 ﻿namespace King.Service.ServiceBus.Unit.Tests
 {
+    using King.Azure.Data;
+    using King.Service.Data;
     using King.Service.ServiceBus;
     using NSubstitute;
     using NUnit.Framework;
-    using System;
 
     [TestFixture]
     public class BusDequeueTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            var queue = Substitute.For<IBusQueue>();
+            new BusDequeue<object>(queue, processor);
+        }
+
+        [Test]
+        public void IsDequeue()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            var queue = Substitute.For<IBusQueue>();
+            Assert.IsNotNull(new BusDequeue<object>(queue, processor) as Dequeue<object>);
+        }
     }
 }
