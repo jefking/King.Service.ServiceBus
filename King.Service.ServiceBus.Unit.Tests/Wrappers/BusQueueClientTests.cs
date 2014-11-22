ba0d5fbd53f45b194b734861b3a836e26734ba34
Jef King <jef@agilebusinesscloud.com>
2014-11-22 00:34:59 -0800

    Testing

---
+++ King.Service.ServiceBus.Unit.Tests/Wrappers/BusQueueClientTests.cs
@@ -0,0 +1,27 @@
+﻿namespace King.Service.ServiceBus.Unit.Tests.Wrappers
+{
+    using King.Service.ServiceBus.Wrappers;
+    using Microsoft.ServiceBus.Messaging;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class BusQueueClientTests
+    {
+        const string connection = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+
+        [Test]
+        public void Constructor()
+        {
+            var name = Guid.NewGuid().ToString();
+            new BusQueueClient(QueueClient.CreateFromConnectionString(connection, name));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorQueueClientNull()
+        {
+            new BusQueueClient(null);
+        }
+    }
+}
