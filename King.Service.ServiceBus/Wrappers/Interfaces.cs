6dd07e0a836572c04c4438b66b12ea3f25937f4f
Jef King <jef@agilebusinesscloud.com>
2014-11-22 00:46:02 -0800

    Comments

--- King.Service.ServiceBus/Wrappers/Interfaces.cs
+++ King.Service.ServiceBus/Wrappers/Interfaces.cs
@@ -5,12 +5,39 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Bus Queue Client Wrapper
+    /// </summary>
     public interface IBusQueueClient
     {
         #region Methods
+        /// <summary>
+        /// Send
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <returns>Task</returns>
         Task Send(BrokeredMessage message);
+
+        /// <summary>
+        /// Recieve
+        /// </summary>
+        /// <param name="serverWaitTime">Server Wait Time</param>
+        /// <returns>Brokered Message</returns>
         Task<BrokeredMessage> Receive(TimeSpan serverWaitTime);
+
+        /// <summary>
+        /// Recieve Batch
+        /// </summary>
+        /// <param name="messageCount">Message Count</param>
+        /// <param name="serverWaitTime">Server Wait Time</param>
+        /// <returns>Brokered Messages</returns>
         Task<IEnumerable<BrokeredMessage>> ReceiveBatch(int messageCount, TimeSpan serverWaitTime);
+
+        /// <summary>
+        /// On Message
+        /// </summary>
+        /// <param name="callback">Call Back</param>
+        /// <param name="options">Options</param>
         void OnMessage(Func<BrokeredMessage, Task> callback, OnMessageOptions options);
         #endregion
     }
