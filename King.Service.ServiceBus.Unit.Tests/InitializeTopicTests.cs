e4c69ad3df60cfda6f30d8ed7164e60605bfca5c
Jef King <jef@agilebusinesscloud.com>
2014-11-26 21:44:58 -0800

    Initialize Topics

---
+++ King.Service.ServiceBus.Unit.Tests/InitializeTopicTests.cs
@@ -0,0 +1,38 @@
+﻿namespace King.Service.ServiceBus.Unit.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class InitializeTopicTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var fake = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+            new InitializeTopic(Guid.NewGuid().ToString(), fake);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionStringNull()
+        {
+            new InitializeTopic(Guid.NewGuid().ToString(), null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorNameNull()
+        {
+            var fake = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+            new InitializeTopic(null, fake);
+        }
+
+        [Test]
+        public void IsInitializeTask()
+        {
+            var fake = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+            Assert.IsNotNull(new InitializeTopic(Guid.NewGuid().ToString(), fake) as InitializeTask);
+        }
+    }
+}
---
+++ King.Service.ServiceBus/InitializeTopic.cs
@@ -0,0 +1,65 @@
+﻿namespace King.Service.ServiceBus
+{
+    using Microsoft.ServiceBus;
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Initialize Topic
+    /// </summary>
+    public class InitializeTopic : InitializeTask
+    {
+        #region Members
+        /// <summary>
+        /// Namespace Manager
+        /// </summary>
+        protected readonly NamespaceManager manager = null;
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        protected readonly string name = null;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="connectionString">Connection String</param>
+        public InitializeTopic(string name, string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name");
+            }
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("connectionString");
+            }
+
+            this.name = name;
+            this.manager = NamespaceManager.CreateFromConnectionString(connectionString);
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run Async
+        /// </summary>
+        /// <returns></returns>
+        public override async Task RunAsync()
+        {
+            //var created = false;
+            var exists = await manager.TopicExistsAsync(name);
+            if (!exists)
+            {
+                await manager.CreateTopicAsync(name);
+                //created = true;
+            }
+
+            //return created;
+        }
+        #endregion
+    }
+}
