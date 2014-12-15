4b6bb362d9df84dcf3c38c117d6b917c4d00e081
Jef King <jef@agilebusinesscloud.com>
2014-12-14 17:37:52 -0800

    Nuget time

--- King.Service.ServiceBus/BusDequeueBatch.cs
+++ King.Service.ServiceBus/BusDequeueBatch.cs
@@ -19,7 +19,7 @@
         /// <param name="processor">Processor</param>
         /// <param name="minimumPeriodInSeconds">Minimum Period In Seconds</param>
         /// <param name="maximumPeriodInSeconds">Maximum Period In Seconds</param>
-        public BusDequeueBatch(IBusQueueReciever queue, IProcessor<T> processor, int batchCount = 5, int minimumPeriodInSeconds = BaseTimes.MinimumStorageTiming, int maximumPeriodInSeconds = BaseTimes.MaximumStorageTiming)
+        public BusDequeueBatch(IBusQueueReciever queue, IProcessor<T> processor, byte batchCount = 5, int minimumPeriodInSeconds = BaseTimes.MinimumStorageTiming, int maximumPeriodInSeconds = BaseTimes.MaximumStorageTiming)
             : base(new ServiceBusQueuePoller<T>(queue, TimeSpan.FromSeconds(minimumPeriodInSeconds)), processor, batchCount, minimumPeriodInSeconds, maximumPeriodInSeconds)
         {
         }
