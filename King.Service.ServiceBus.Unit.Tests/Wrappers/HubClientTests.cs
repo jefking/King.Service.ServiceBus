de09263f2476bbe840197a9d2a13e4f01dc5b8de
Jef King <jef@agilebusinesscloud.com>
2015-04-21 18:47:27 -0700

    Unit Tests

---
+++ King.Service.ServiceBus.Unit.Tests/Wrappers/HubClientTests.cs
@@ -0,0 +1,35 @@
+﻿namespace King.Service.ServiceBus.Unit.Tests.Wrappers
+{
+    using King.Service.ServiceBus.Wrappers;
+    using Microsoft.ServiceBus.Messaging;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class HubClientTests
+    {
+        const string connection = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+
+        [Test]
+        public void Constructor()
+        {
+            var name = Guid.NewGuid().ToString();
+            new HubClient(EventHubClient.CreateFromConnectionString(connection, name));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorQueueClientNull()
+        {
+            new HubClient(null);
+        }
+
+        [Test]
+        public void Client()
+        {
+            var client = EventHubClient.CreateFromConnectionString(connection, "test");
+            var btc = new HubClient(client);
+            Assert.AreEqual(client, btc.Client);
+        }
+    }
+}
