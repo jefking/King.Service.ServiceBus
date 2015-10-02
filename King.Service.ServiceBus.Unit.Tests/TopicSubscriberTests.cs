c03091e4ff968064ff29c5e5abd4d764090b360e
Jef King <jef@agilebusinesscloud.com>
2015-10-01 19:12:01 -0700

    mic

---
+++ King.Service.ServiceBus.Unit.Tests/TopicSubscriberTests.cs
@@ -0,0 +1,43 @@
+﻿namespace King.Service.ServiceBus.Unit.Tests
+{
+    using King.Service.ServiceBus.Wrappers;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class TopicSubscriberTests
+    {
+        const string connection = "Endpoint=sb://test.servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]";
+
+        [Test]
+        public void Constructor()
+        {
+            new TopicSubscriber(Guid.NewGuid().ToString(), connection, Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorNameNull()
+        {
+            new TopicSubscriber(null, connection, Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionNull()
+        {
+            new TopicSubscriber(Guid.NewGuid().ToString(), null, Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorSubscriptionNameNull()
+        {
+            new TopicSubscriber(Guid.NewGuid().ToString(), connection, null);
+        }
+    }
+}
---
+++ King.Service.ServiceBus/TopicSubscriber.cs
@@ -0,0 +1,71 @@
+﻿namespace King.Service.ServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Azure.Data;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
+    /// <summary>
+    /// Topic Subscriber
+    /// </summary>
+    public class TopicSubscriber
+    {
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Queue Name</param>
+        /// <param name="connection">Connection String</param>
+        /// <param name="subscriptionName">Subsciption Name</param>
+        /// <param name="sqlFilter">SQL Query (null = all)</param>
+        public TopicSubscriber(string name, string connection, string subscriptionName, string sqlFilter = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name");
+            }
+            if (string.IsNullOrWhiteSpace(connection))
+            {
+                throw new ArgumentException("connection");
+            }
+            if (string.IsNullOrWhiteSpace(subscriptionName))
+            {
+                throw new ArgumentException("subscriptionName");
+            }
+
+            var nm = NamespaceManager.CreateFromConnectionString(connection);
+
+            //if (string.IsNullOrWhiteSpace(sqlFilter))
+            //{
+            //    if (!nm.SubscriptionExists(name, subscriptionName))
+            //    {
+            //        nm.CreateSubscription(name, subscriptionName);
+            //    }
+            //}
+            //else
+            //{
+            //    if (!nm.SubscriptionExists(name, subscriptionName))
+            //    {
+            //        nm.CreateSubscription(name, subscriptionName, new SqlFilter(sqlFilter));
+            //    }
+            //}
+        }
+        #endregion
+    }
+
+    public class TopicPoller<T> : IPoller<T>
+    {
+        public Task<IQueued<T>> Poll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IQueued<T>>> PollMany(int messageCount = 5)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
