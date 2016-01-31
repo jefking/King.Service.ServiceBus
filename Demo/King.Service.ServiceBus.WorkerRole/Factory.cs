d46bb78b2f79bd49a8b5a3dd6d330e519015ac5b
Jef King <jefkin@microsoft.com>
2016-01-31 14:57:30 -0800

    simplify demo

--- Demo/King.Service.ServiceBus.WorkerRole/Factory.cs
+++ Demo/King.Service.ServiceBus.WorkerRole/Factory.cs
@@ -1,14 +1,13 @@
 ﻿namespace King.Service.ServiceBus
 {
-    using System.Collections.Generic;
     using King.Service.Data;
     using King.Service.ServiceBus.Queue;
     using King.Service.WorkerRole;
     using King.Service.WorkerRole.Models;
     using King.Service.WorkerRole.Queue;
-    using King.Service.WorkerRole.Scalable;
     using King.Service.WorkerRole.Topic;
-    
+    using System.Collections.Generic;
+
     public class Factory : ITaskFactory<Configuration>
     {
         /// <summary>
@@ -19,42 +18,26 @@
         public IEnumerable<IRunnable> Tasks(Configuration config)
         {
             //Connections
-            var pollReceiver = new BusQueueReciever(config.PollingName, config.Connection);
-            var scalingQueue = new BusQueueReciever(config.ScalingQueueName, config.Connection);
             var eventReciever = new BusQueueReciever(config.EventsName, config.Connection);
             var bufferReciever = new BusQueueReciever(config.BufferedEventsName, config.Connection);
-            var dynamicReciever = new BusQueueReciever(config.DynamicQueueName, config.Connection);
 
             //Initialize Service Bus
-            yield return new InitializeBusQueue(pollReceiver);
-            yield return new InitializeBusQueue(scalingQueue);
             yield return new InitializeBusQueue(eventReciever);
             yield return new InitializeBusQueue(bufferReciever);
-            yield return new InitializeBusQueue(dynamicReciever);
             yield return new InitializeTopic(config.TopicName, config.Connection);
-
-            //Polling Dequeue Runner
-            yield return new AdaptiveRunner(new BusDequeue<ExampleModel>(pollReceiver, new ExampleProcessor()));
-
+            
             //Task for watching for queue events
             yield return new BusEvents<ExampleModel>(eventReciever, new EventHandler());
 
             //Task for recieving queue events to specific times
             yield return new BufferedReciever<ExampleModel>(bufferReciever, new EventHandler());
-
-            //Auto Scaling Dequeue Task
-            yield return new ScalableQueue(scalingQueue, config);
-
-            //Auto Batch Size Dequeue Task
-            yield return new AdaptiveRunner(new BusDequeueBatchDynamic<ExampleModel>(config.DynamicQueueName, config.Connection, new ExampleProcessor()));
-
+            
             //Dynamic Batch Size, Frequency, Threads (and queue creation)
             var factory = new BusDequeueFactory(config.Connection);
-            var setup = new QueueSetup<ExampleModel>
+            var setup = new QueueSetupProcessor<ExampleProcessor, ExampleModel>
             {
                 Name = config.FactoryQueueName,
                 Priority = QueuePriority.Medium,
-                Processor = () => { return new ExampleProcessor(); },
             };
 
             foreach (var t in factory.Tasks<ExampleModel>(setup))
@@ -65,8 +48,6 @@
             //Simulate messages being added to queues
             yield return new QueueForAction(new BusQueueSender(config.PollingName, config.Connection), "Poll");
             yield return new QueueForAction(new BusQueueSender(config.EventsName, config.Connection), "Event");
-            yield return new QueueForAction(new BusQueueSender(config.ScalingQueueName, config.Connection), "Scaling");
-            yield return new QueueForAction(new BusQueueSender(config.DynamicQueueName, config.Connection), "Dynamic");
             yield return new QueueForAction(new BusQueueSender(config.FactoryQueueName, config.Connection), "Factory");
             yield return new QueueForBuffer(new BusQueueSender(config.BufferedEventsName, config.Connection));
 
