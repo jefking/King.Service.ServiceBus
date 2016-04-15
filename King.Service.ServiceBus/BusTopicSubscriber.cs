9f2c16270017f63ab5193f6c045d4f3c3f49b7e9
Jef King <jefkin@microsoft.com>
2016-04-14 22:45:43 -0700

    Subscription Creation for Topics

--- King.Service.ServiceBus/BusTopicSubscriber.cs
+++ King.Service.ServiceBus/BusTopicSubscriber.cs
@@ -1,32 +1,49 @@
 ﻿namespace King.Service.ServiceBus
 {
-    using System;
-    using System.Threading.Tasks;
+    using Azure.Data;
     using Microsoft.ServiceBus;
     using Microsoft.ServiceBus.Messaging;
+    using System;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Topic Subscriber
     /// </summary>
-    public class BusTopicSubscriber
+    public class BusTopicSubscriber : IAzureStorage
     {
         #region Members
         /// <summary>
-        /// Subscription Description
+        /// Topic Name
         /// </summary>
-        protected readonly SubscriptionDescription desciption;
+        protected readonly string topicName = null;
+
+        /// <summary>
+        /// Namespace Manager
+        /// </summary>
+        protected readonly NamespaceManager manager = null;
+
+        /// <summary>
+        /// Subscription Name
+        /// </summary>
+        protected readonly string subscriptionName;
+
+        /// <summary>
+        /// SQL Filter
+        /// </summary>
+        protected readonly string sqlFilter;
         #endregion
 
         #region Constructors
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <param name="name">Queue Name</param>
+        /// <param name="topicName">Topic Name</param>
         /// <param name="connection">Connection String</param>
         /// <param name="subscriptionName">Subsciption Name</param>
         /// <param name="sqlFilter">SQL Query (null = all)</param>
-        public BusTopicSubscriber(string name, string connection, string subscriptionName, string sqlFilter = null)
+        public BusTopicSubscriber(string topicName, string connection, string subscriptionName, string sqlFilter = null)
         {
-            if (string.IsNullOrWhiteSpace(name))
+            if (string.IsNullOrWhiteSpace(topicName))
             {
                 throw new ArgumentException("name");
             }
@@ -39,25 +56,67 @@
                 throw new ArgumentException("subscriptionName");
             }
 
-            var nm = NamespaceManager.CreateFromConnectionString(connection);
+            this.topicName = topicName;
+            this.subscriptionName = subscriptionName;
+            this.sqlFilter = string.IsNullOrWhiteSpace(sqlFilter) ? null : sqlFilter;
 
-            this.desciption = this.Bind(nm, name, subscriptionName, sqlFilter).Result;
+            this.manager = NamespaceManager.CreateFromConnectionString(connection);
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Subscription Name
+        /// </summary>
+        public virtual string Name
+        {
+            get
+            {
+                return this.subscriptionName;
+            }
+        }
+
+        /// <summary>
+        /// Topic Name
+        /// </summary>
+        public virtual string TopicName
+        {
+            get
+            {
+                return this.topicName;
+            }
         }
         #endregion
 
         #region Methods
-        public virtual async Task<SubscriptionDescription> Bind(NamespaceManager nm, string name, string subscriptionName, string sqlFilter)
+        /// <summary>
+        /// Create If Not Exists
+        /// </summary>
+        /// <returns>Created</returns>
+        public virtual async Task<bool> CreateIfNotExists()
         {
+            var created = false;
+            if (!await this.manager.SubscriptionExistsAsync(topicName, subscriptionName))
+            {
+                var d = (string.IsNullOrWhiteSpace(sqlFilter)) ?
+                    await this.manager.CreateSubscriptionAsync(topicName, subscriptionName) :
+                    await this.manager.CreateSubscriptionAsync(topicName, subscriptionName, new SqlFilter(sqlFilter));
 
-            if (!await nm.SubscriptionExistsAsync(name, subscriptionName))
-            {
-                return (string.IsNullOrWhiteSpace(sqlFilter)) ?
-                    await nm.CreateSubscriptionAsync(name, subscriptionName) :
-                    await nm.CreateSubscriptionAsync(name, subscriptionName, new SqlFilter(sqlFilter));
+                created = d.Status == EntityStatus.Creating;
             }
-            else
+
+            return created;
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <returns>Task</returns>
+        public async Task Delete()
+        {
+            if (!await this.manager.SubscriptionExistsAsync(topicName, subscriptionName))
             {
-                return await nm.GetSubscriptionAsync(name, subscriptionName);
+                await this.manager.DeleteSubscriptionAsync(this.topicName, this.topicName);
             }
         }
         #endregion
