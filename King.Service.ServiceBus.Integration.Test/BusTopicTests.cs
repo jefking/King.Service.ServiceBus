5303669b3fff8e854d61bbf4e07b7566af71c31d
Jef King <jef@agilebusinesscloud.com>
2016-04-14 19:36:30 -0700

    nu-release

--- King.Service.ServiceBus.Integration.Test/BusTopicTests.cs
+++ King.Service.ServiceBus.Integration.Test/BusTopicTests.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Configuration;
     using System.Threading.Tasks;
+    using Azure.Data;
     using NUnit.Framework;
 
     [TestFixture]
@@ -10,49 +11,54 @@
     {
         private static readonly string connection = ConfigurationSettings.AppSettings["Microsoft.ServiceBus.ConnectionString"];
 
+        IAzureStorage topic;
+
+        [SetUp]
+        public void Setup()
+        {
+            var random = new Random();
+            var name = string.Format("a{0}b", random.Next());
+
+            topic = new BusTopic(name, connection);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            topic.Delete().Wait();
+        }
+
         [Test]
         public async Task Create()
         {
-            var random = new Random();
-            var name = "a" + random.Next() + "b";
-            var init = new BusTopic(name, connection);
-            var e = await init.CreateIfNotExists();
+            var e = await topic.CreateIfNotExists();
             Assert.IsTrue(e);
         }
 
         [Test]
         public async Task CreateTwice()
         {
-            var random = new Random();
-            var name = "a" + random.Next() + "b";
-            var init = new BusTopic(name, connection);
-            var e = await init.CreateIfNotExists();
+            var e = await topic.CreateIfNotExists();
             Assert.IsTrue(e);
-            e = await init.CreateIfNotExists();
+            e = await topic.CreateIfNotExists();
             Assert.IsFalse(e);
         }
 
         [Test]
         public async Task Delete()
         {
-            var random = new Random();
-            var name = "a" + random.Next() + "b";
-            var init = new BusTopic(name, connection);
-            var e = await init.CreateIfNotExists();
+            var e = await topic.CreateIfNotExists();
             Assert.IsTrue(e);
-            await init.Delete();
+            await topic.Delete();
         }
 
         [Test]
         public async Task DeleteTwice()
         {
-            var random = new Random();
-            var name = "a" + random.Next() + "b";
-            var init = new BusTopic(name, connection);
-            var e = await init.CreateIfNotExists();
+            var e = await topic.CreateIfNotExists();
             Assert.IsTrue(e);
-            await init.Delete();
-            await init.Delete();
+            await topic.Delete();
+            await topic.Delete();
         }
     }
 }
