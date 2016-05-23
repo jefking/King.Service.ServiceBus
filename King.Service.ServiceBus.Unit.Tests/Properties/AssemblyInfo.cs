07aca13eb1a136a31cd542ecb103519b08d0c569
Jef King <jef@agilebusinesscloud.com>
2016-05-23 14:22:25 -0700

    New Version coming

--- King.Service.ServiceBus.Unit.Tests/Properties/AssemblyInfo.cs
+++ King.Service.ServiceBus.Unit.Tests/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3c3020b4-1d4e-4d54-84b2-e1ae1ac63e1f")]
-[assembly: AssemblyVersion("1.0.1.9")]
-[assembly: AssemblyFileVersion("1.0.1.9")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
