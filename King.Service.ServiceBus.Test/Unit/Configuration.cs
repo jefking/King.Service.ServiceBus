acd574f6bc8d617f0b8d56b906378692bb598ed7
Jef King <jefkin@microsoft.com>
2016-08-29 10:38:41 -0700

    new key

--- King.Service.ServiceBus.Test/Unit/Configuration.cs
+++ King.Service.ServiceBus.Test/Unit/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public class Configuration
     {
-        public const string ConnectionString = "Endpoint=sb://kinger.servicebus.windows.net/;SharedAccessKeyName=oss;SharedAccessKey=xixl3KqBe3faRAMH++AoqAKs2KkSAzB2m6dxTLQkJPI=";
+        public const string ConnectionString = "Endpoint=sb://kinger.servicebus.windows.net/;SharedAccessKeyName=oss;SharedAccessKey=efYHLmvDCGtL9d1FEBpA3OzkE1WEQAZTmmzYpki2jr8=";
     }
 }
