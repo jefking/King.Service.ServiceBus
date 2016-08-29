936af31ef3262209e0fe804b113536cb61f9decd
Jef King <jefkin@microsoft.com>
2016-08-29 07:50:04 -0700

    test project

---
+++ King.Service.ServiceBus.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace King.Service.ServiceBus.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
