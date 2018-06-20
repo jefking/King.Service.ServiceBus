80942f7cb51f1b48e653765c9315dc58f53c62fc
Jef King <jefkin@microsoft.com>
2018-06-19 18:01:08 -0700

    switching goodness

---
+++ King.Service.ServiceBus.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace King.Service.ServiceBus.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- King.Service.ServiceBus.Test/Unit/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace King.Service.ServiceBus.Test.Unit
-{
-    public class Configuration
-    {
-        public const string ConnectionString = "Endpoint=sb://kinger.servicebus.windows.net/;SharedAccessKeyName=oss;SharedAccessKey=YOt7IExAbFLRvRi8gvRxeuxaMUHtIwVdFdUiGuMMIO4=";
-    }
-}
