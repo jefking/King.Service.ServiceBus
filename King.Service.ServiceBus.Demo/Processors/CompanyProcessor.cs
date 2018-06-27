5c4195bb211f1680db31fb23a89a4b60d39e8a47
Jef King <jefkin@microsoft.com>
2018-06-27 13:58:14 -0700

    classes for wiring

---
+++ King.Service.ServiceBus.Demo/Processors/CompanyProcessor.cs
@@ -0,0 +1,20 @@
+namespace King.Service.ServiceBus.Demo.Processors
+{
+    using global::Azure.Data.Wrappers;
+    using King.Service.ServiceBus.Demo.Models;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Company Processor, occurs post dequeue action
+    /// </summary>
+    public class CompanyProcessor : IProcessor<CompanyModel>
+    {
+        public Task<bool> Process(CompanyModel data)
+        {
+            Trace.TraceInformation("Save company to data store: '{0}/{1}'", data.Name, data.Id);
+
+            return Task.FromResult<bool>(true);
+        }
+    }
+}
