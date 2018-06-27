a7c80b02576167a210b1a4c82a55349f3af19d50
Jef King <jefkin@microsoft.com>
2018-06-27 13:45:41 -0700

    comments

--- King.Service.ServiceBus.Demo/Program.cs
+++ King.Service.ServiceBus.Demo/Program.cs
@@ -6,17 +6,22 @@
     {
         public static void Main(string[] args)
         {
+            // Appication Configuration Settings
             var config = new AppConfig()
             {
                 ConnectionString = "",
             };
 
+            // Start tasks
             using (var manager = new RoleTaskManager<AppConfig>(new TaskFactory()))
             {
+                // On Start
                 manager.OnStart(config);
 
+                // Run
                 manager.Run();
-
+                
+                // Pin loading thread
                 while (true)
                 {
                     Thread.Sleep(1500);
