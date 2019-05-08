0b839590ff35790e6413f723315274516039f621
jef king <jef@microsoft.com>
2019-05-08 09:29:03 -0700

    fixing demo

--- King.Service.ServiceBus.Demo/Program.cs
+++ King.Service.ServiceBus.Demo/Program.cs
@@ -1,17 +1,27 @@
 ﻿namespace King.Service.ServiceBus.Demo
 {
+    using System;
+    using System.Diagnostics;
     using System.Threading;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Appication Configuration Settings
-            var config = new AppConfig()
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            foreach (var arg in args)
             {
-                ConnectionString = "",
+                Trace.TraceInformation("arg: {0}", arg);
+            }
+
+            // Load Config
+            var config = new AppConfig
+            {
+                ConnectionString = args[0],
                 QueueName = "Company"
             };
+
+            Trace.TraceInformation("Connection String: {0}", config.ConnectionString);
 
             // Start tasks
             using (var manager = new RoleTaskManager<AppConfig>(new TaskFactory()))
