12feb88d72e3d02a4a15c04cdabad3cf1e5eb4d0
jef king <jef@microsoft.com>
2019-05-08 13:11:05 -0700

    remove files

--- King.Service.ServiceBus.Tests/Unit/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace King.Service.ServiceBus.Test.Unit
-{
-    public class Configuration
-    {
-        public const string ConnectionString = "Endpoint=sb://x.servicebus.windows.net/;SharedAccessKeyName=oss;SharedAccessKey=YOt7IExAbFLRvRi8gvRxeuxaMUHtIwVdFdUiGuMMIO4=";
-    }
-}
