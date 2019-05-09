9de8b7a48e7ac3616ec37301b178ad69228a50a8
jef king <jef@microsoft.com>
2019-05-09 16:25:56 -0700

    conn string

--- King.Service.ServiceBus.Tests/Integration/Configuration.cs
+++ King.Service.ServiceBus.Tests/Integration/Configuration.cs
@@ -2,6 +2,6 @@ namespace King.Service.ServiceBus.Test.Integration
 {
     public class Configuration
     {
-        public const string ConnectionString = "";
+        public const string ConnectionString = "Endpoint=sb://kingservice.servicebus.windows.net/;SharedAccessKeyName=sendrecieve;SharedAccessKey=2QV3dej9PIXHs8l/O86PjtNOXkfwCLg8qQOoB+5wtMU=";
     }
 }
