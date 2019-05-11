743149e430757b1cfe413b9bea217237ffab4f7c
jef king <jef@microsoft.com>
2019-05-10 20:20:35 -0700

    adding send to queue

---
+++ King.Service.ServiceBus.Tests/Integration/Wrappers/BusTopicClientTests.cs
@@ -0,0 +1,67 @@
+namespace King.Service.ServiceBus.Test.Integration.Wrappers
+{
+    using global::Azure.Data.Wrappers;
+    using King.Service.ServiceBus.Models;
+    using King.Service.ServiceBus.Wrappers;
+    using Microsoft.Azure.ServiceBus;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class BusTopicClientTests
+    {
+        private static readonly string connection = King.Service.ServiceBus.Test.Integration.Configuration.ConnectionString;
+
+        private string sendName, recieveName, sendBatchName;
+
+        [SetUp]
+        public void Setup()
+        {
+            var random = new Random();
+            sendName = string.Format("a{0}b", random.Next());
+            recieveName = string.Format("a{0}b", random.Next());
+            sendBatchName = string.Format("a{0}b", random.Next());
+
+            var client = new BusManagementClient(connection);
+            Task.WaitAll(
+                new Task[] {
+                    client.TopicCreate(sendName)
+                    , client.TopicCreate(recieveName)
+                    , client.TopicCreate(sendBatchName)
+                }
+            );
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var client = new BusManagementClient(connection).Client;
+            Task.WaitAll(
+                new Task[] {
+                    client.DeleteTopicAsync(sendName)
+                    , client.DeleteTopicAsync(recieveName)
+                    , client.DeleteTopicAsync(sendBatchName)
+                }
+            );
+        }
+
+        [Test]
+        public async Task Send()
+        {
+            var queue = new BusTopicClient(sendName, connection);
+            await queue.Send(new Message());
+        }
+
+        [Test]
+        public async Task SendBatch()
+        {
+            var msgs = new Message[] { new Message(), new Message(), new Message(), new Message() };
+
+            var queue = new BusTopicClient(sendBatchName, connection);
+            await queue.Send(msgs);
+        }
+    }
+}
---
+++ King.Service.ServiceBus/Wrappers/BusTopicClient.cs
@@ -0,0 +1,88 @@
+namespace King.Service.ServiceBus.Wrappers
+{
+    using Microsoft.Azure.ServiceBus;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Bus Topic Client Wrapper
+    /// </summary>
+    public class BusTopicClient : IBusTopicClient
+    {
+        #region Members
+        /// <summary>
+        /// Topic Client
+        /// </summary>
+        protected readonly ITopicClient client = null;
+
+        /// <summary>
+        /// Encoding Property Key
+        /// </summary>
+        public const string EncodingKey = "encoding";
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="connection">Connetion</param>
+        public BusTopicClient(string name, string connection)
+            : this(new TopicClient(connection, name))
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="client">Topic Client</param>
+        public BusTopicClient(ITopicClient client)
+        {
+            if (null == client)
+            {
+                throw new ArgumentNullException("client");
+            }
+
+            this.client = client;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Topic Client
+        /// </summary>
+        public virtual ITopicClient Client
+        {
+            get
+            {
+                return this.client;
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Send
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <returns>Task</returns>
+        public virtual async Task Send(Message message)
+        {
+            await this.client.SendAsync(message);
+        }
+
+        /// <summary>
+        /// Send Batch
+        /// </summary>
+        /// <param name="message">Messages</param>
+        /// <returns>Task</returns>
+        public virtual async Task Send(IEnumerable<Message> messages)
+        {
+            await this.client.SendAsync(messages.ToList());
+        }
+        #endregion
+    }
+}
